b8e94c0a474af623284c39814f51cb29b7f53580
halaprix <halaprix@users.noreply.github.com>
2026-06-16 20:13:18 +0200

    chore: bump version to 0.2.0-alpha.1

--- phone/app/build.gradle.kts
+++ phone/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.halaprix.leakwatch"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0-alpha.1"
+        versionCode = 2
+        versionName = "0.2.0-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
